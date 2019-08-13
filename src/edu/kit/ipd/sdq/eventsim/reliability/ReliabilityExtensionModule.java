1a85f874f8018591e02bc512d6297b6813277515
Sandro <layornos@gmail.com>
2019-08-13 10:57:42 +0200

    create reliability module

---
+++ src/edu/kit/ipd/sdq/eventsim/reliability/ReliabilityExtensionModule.java
@@ -0,0 +1,13 @@
+package edu.kit.ipd.sdq.eventsim.reliability;
+
+import com.google.inject.AbstractModule;
+
+public class ReliabilityExtensionModule extends AbstractModule {
+
+	@Override
+	protected void configure() {
+		// TODO Auto-generated method stub
+		
+	}
+}
+ 
---
+++ src/edu/kit/ipd/sdq/eventsim/reliability/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author layornos
+ *
+ */
+package edu.kit.ipd.sdq.eventsim.reliability;
