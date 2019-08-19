b2e010f963bf196fe0e91d92ac2ebf06fb02cd5f
Sandro <layornos@gmail.com>
2019-08-19 17:19:07 +0200

    update

---
+++ src/edu/kit/ipd/sdq/eventsim/reliability/strategies/ResourceFailureDecoratingSimulationStrategy.java
@@ -0,0 +1,40 @@
+package edu.kit.ipd.sdq.eventsim.reliability.strategies;
+
+import java.util.function.Consumer;
+import org.apache.log4j.Logger;
+import org.palladiosimulator.pcm.seff.AbstractAction;
+
+import com.google.inject.Inject;
+
+import edu.kit.ipd.sdq.eventsim.api.ISimulationConfiguration;
+import edu.kit.ipd.sdq.eventsim.api.ISimulationMiddleware;
+import edu.kit.ipd.sdq.eventsim.interpreter.DecoratingSimulationStrategy;
+import edu.kit.ipd.sdq.eventsim.interpreter.SimulationStrategy;
+import edu.kit.ipd.sdq.eventsim.interpreter.TraversalInstruction;
+import edu.kit.ipd.sdq.eventsim.system.entities.Request;
+
+public class ResourceFailureDecoratingSimulationStrategy
+		implements DecoratingSimulationStrategy<AbstractAction, Request> {
+
+	private static final Logger logger = Logger.getLogger(ResourceFailureDecoratingSimulationStrategy.class);
+	
+    @Inject
+    private ISimulationConfiguration configuration;
+
+    @Inject
+    private ISimulationMiddleware middleware;
+
+	private SimulationStrategy<AbstractAction, Request> decorated;
+
+	@Override
+	public void decorate(SimulationStrategy<AbstractAction, Request> decorated) {
+		this.decorated = decorated;
+	}
+
+	@Override
+	public void simulate(AbstractAction action, Request entity, Consumer<TraversalInstruction> onFinishCallback) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
